d39b4fd7a4ff4b84da84311bd6e3f977ddd8a922
ajaykrishna2013 <ajaykrishna@gmail.com>
2017-07-02 20:22:09 -0700

    all pyc files removed

--- anomaly_detection/insight_testsuite/temp/src/detect_anomaly.py
+++ anomaly_detection/insight_testsuite/temp/src/detect_anomaly.py
@@ -3,21 +3,29 @@ import sys
 import os.path
 from social_network import SocialNetwork
 from users import User
+import logging
+
+index = 0
 
 
-def process_batch_input(social_network, batch_input_file):
-	with open('.' + batch_input_file, 'r') as f:
+def process_batch_input(batch_input_file, output_log, logger):
+	global index
+
+	#with open('.' + batch_input_file, 'r') as f:
+	with open(batch_input_file, 'r') as f:
 		for idx, line in enumerate(f.readlines()):
+			logger.info('File %s: Processing line numer: %d', batch_input_file, idx)
 			line.rstrip()
 			json_line = json.loads(line)
 			if 'D' in json_line:
 				degree = json_line['D']
 				num_tracked = json_line['T']
-				social_network = SocialNetwork(degree)
+				logger.info('Degree of network = %s number of tracked purchases = %s', degree, num_tracked)
+				social_network = SocialNetwork(output_log, degree, num_tracked)
 			elif json_line['event_type'] == 'purchase':
 				timestamp = json_line['timestamp']
-				purchase_amount = json_line['amount']
-				purchase = (purchase_amount, timestamp)
+				purchase_amount = float(json_line['amount'])
+				purchase = (purchase_amount, timestamp, index)
 				user_id = json_line['id']
 
 				if not user_id in social_network.network:
@@ -36,35 +44,65 @@ def process_batch_input(social_network, batch_input_file):
 				timestamp = json_line['timestamp']
 				event_type = json_line['event_type']
 
-				if (user1_id in social_network.network or user2_id in social_network.network):
+				if event_type == 'befriend':
+					social_network.add_friend(user1_id, user2_id)
+				elif event_type == 'unfriend':
+					social_network.remove_friend(user1_id, user2_id)
+			index += 1
+	return social_network
+
+
+def process_event_input(social_network, event_input_file,logger):
+	global index
+
+	#with open('.' + event_input_file, 'r') as f:
+	with open(event_input_file, 'r') as f:
+		for idx, line in enumerate(f.readlines()):
+			logger.info('File %s: Processing line numer: %d', event_input_file, idx)
+			line.rstrip()
+			try:
+				json_line = json.loads(line)
+
+				if json_line['event_type'] == 'purchase':
+					timestamp = json_line['timestamp']
+					purchase_amount = float(json_line['amount'])
+					purchase = (purchase_amount, timestamp, index)
+					user_id = json_line['id']
+					social_network.check_anamoly_and_update_network(user_id, purchase)
+				elif json_line['event_type'] in ['befriend', 'unfriend']:
+					user1_id = json_line['id1']
+					user2_id = json_line['id2']
+					timestamp = json_line['timestamp']
+					event_type = json_line['event_type']
 
 					if event_type == 'befriend':
-						if user1_id in social_network.network and not user2_id in social_network.network:
-							# create user
-							user2 = User(user2_id)
-							social_network.add_user(user2)
-							social_network.add_friend(user1_id, user2_id)
-						elif not user1_id in social_network.network and user2_id in social_network.network:
-							# create user
-							user1 = User(user1_id)
-							social_network.add_user(user1)
-							social_network.add_friend(user1_id, user2_id)
-						else:
-							social_network.add_friend(user1_id, user2_id)
+						social_network.add_friend(user1_id, user2_id)
 					elif event_type == 'unfriend':
 						social_network.remove_friend(user1_id, user2_id)
+			except ValueError:
+				logger.info('File %s: Processing line: %s', event_input_file, line)
+			index += 1
 
 
-if __name__ =='__main__':
+def main():
 	batch_input, event_input, output_log = sys.argv[1:]
 	print 'batch input', batch_input
-	print 'envent input', event_input
+	print 'event input', event_input
 	print 'output_log', output_log
 
+	logger = logging.getLogger(__name__)
 	num_tracked = 0
 	degree = 0
 
-	social_network = None
-	process_batch_input(social_network, batch_input)
+	social_network = process_batch_input(batch_input, output_log, logger)
+	process_event_input(social_network, event_input, logger)
+
+
+if __name__ =='__main__':
+	logging.basicConfig(level=logging.DEBUG,
+						filename='anomaly_detection.log',
+						format='%(name)s: %(message)s')
+	main()
+
 
 
