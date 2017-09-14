bdf5c0ec0e01990288115f5684b8867a9a2af929
Ajay <ajaykrishna@gmail.com>
2017-09-14 21:02:19 +0000

    Add project folder

--- CEPAnalytics/data/producer.py
+++
@@ -1,7 +0,0 @@
-from confluent_kafka import Producer
-
-#p = Producer({'bootstrap.servers':'127.0.0.1:9092'})
-p = Producer({'bootstrap.servers':'54.69.161.38:9092'})
-for i in range(200,210,1):
-	p.produce('first_topic', 'text from python proc'+str(i), str(i))
-p.flush()
