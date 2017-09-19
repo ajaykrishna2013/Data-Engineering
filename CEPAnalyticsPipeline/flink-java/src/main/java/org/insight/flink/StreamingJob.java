09108b8267f207c39b0d23270f6e5be84419cc2f
ajaykrishna2013 <ajaykrishna@gmail.com>
2017-09-18 19:15:14 -0700

    rename project folder to fix maven dependency

---
+++ CEPAnalyticsPipeline/flink-java/src/main/java/org/insight/flink/StreamingJob.java
@@ -0,0 +1,123 @@
+
+package org.insight.flink;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Properties;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
+//Cassandra Stuff
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
+import org.apache.flink.streaming.connectors.cassandra.ClusterBuilder;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Cluster.Builder;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
+
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class StreamingJob {
+	private static final String INSERT = "INSERT INTO test.writetuple (element1, element2) VALUES (?, ?)";
+	private static final ArrayList<Tuple2<String, Integer>> collection = new ArrayList<>(20);
+
+	static {
+		for (int i = 0; i < 20; i++) {
+			collection.add(new Tuple2<>("event 1-" + i, i));
+		}
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		// set up the streaming execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		
+		Properties properties = new Properties();
+		properties.setProperty("bootstrap.servers", "52.24.35.74:9092");
+		properties.setProperty("zookeeper.connect", "52.24.35.74:2181");
+		properties.setProperty("group.id", "flink_consumer");
+		
+		DataStream<String> stream = env.addSource(new FlinkKafkaConsumer09<>( "SmartHome1", new SimpleStringSchema(), properties) );
+		
+		//writeElastic(stream);	
+
+		stream.map(new MapFunction<String, String>() {
+		  private static final long serialVersionUID = -6867736771747690202L;
+		
+		  @Override
+		  public String map(String value) throws Exception {
+		    return "Stream Value: " + value;
+		  }
+		}).print();
+
+		// Adding cassandra sink
+		DataStreamSource<Tuple2<String, Integer>> source = env.fromCollection(collection);
+
+		CassandraSink.addSink(source)
+			.setQuery(INSERT)
+			.setClusterBuilder(new ClusterBuilder() {
+				@Override
+				protected Cluster buildCluster(Builder builder) {
+					return builder.addContactPoint("10.0.0.10").build();
+				}
+			})
+			.build();
+
+		env.execute("WriteTupleIntoCassandra");
+		// Added cassandra sink
+
+
+
+		/**
+		 * Here, you can start creating your execution plan for Flink.
+		 *
+		 * Start with getting some data from the environment, like
+		 * 	env.readTextFile(textPath);
+		 *
+		 * then, transform the resulting DataStream<String> using operations
+		 * like
+		 * 	.filter()
+		 * 	.flatMap()
+		 * 	.join()
+		 * 	.coGroup()
+		 *
+		 * and many more.
+		 * Have a look at the programming guide for the Java API:
+		 *
+		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
+		 *
+		 */
+
+		// execute program
+		env.execute("Flink Streaming Java API Skeleton");
+	}
+}
