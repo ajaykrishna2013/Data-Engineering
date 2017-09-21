039471c72ddf4cf63950dfd2c1e20041321443a4
ajaykrishna2013 <ajaykrishna@gmail.com>
2017-09-21 15:58:11 +0000

    add first working cep logic

--- CEPAnalyticsPipeline/flink-java/src/main/java/org/insight/flink/StreamingJob.java
+++ CEPAnalyticsPipeline/flink-java/src/main/java/org/insight/flink/StreamingJob.java
@@ -18,12 +18,31 @@ package org.insight.flink;
  * limitations under the License.
  */
 
+
+import org.apache.flink.api.java.tuple.*;
+import org.apache.flink.cep.CEP;
+import org.apache.flink.cep.PatternSelectFunction;
+import org.apache.flink.cep.PatternFlatSelectFunction;
+import org.apache.flink.cep.PatternFlatTimeoutFunction;
+import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.SimpleCondition;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.types.Either;
+
 import java.util.Properties;
 import com.google.gson.Gson;
 import org.apache.flink.api.common.functions.*;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
+//import org.apache.flink.streaming.api.functions.sink;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+import org.apache.flink.streaming.api.functions.IngestionTimeExtractor;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
@@ -32,9 +51,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Random;
 //Cassandra Stuff
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.api.java.tuple.Tuple6;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
 import org.apache.flink.streaming.connectors.cassandra.ClusterBuilder;
@@ -56,61 +72,162 @@ import java.util.Map;
 public class StreamingJob {
 	private static final String INSERT_SMART_HOME_USAGE_GENERATION = "INSERT INTO cep_analytics.smarthome_usage_gen_table (home_id, generation, usage, event_time) VALUES (?, ?, ?, ?)";
 	private static final String INSERT_CEP = "INSERT INTO cep_analytics.smarthome_cep_table (home_id, event_time, event_description, event_severity, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?)";
-	private static final ArrayList<Tuple2<String, Integer>> collection = new ArrayList<>(20);
-
-	static {
-		for (int i = 0; i < 20; i++) {
-			collection.add(new Tuple2<>("event 1-" + i, i));
-		}
-	}
-
 
 	public static void main(String[] args) throws Exception {
 		// set up the streaming execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+		env.enableCheckpointing(1000);
 		
 		Properties properties = new Properties();
 		properties.setProperty("bootstrap.servers", "52.24.35.74:9092");
 		properties.setProperty("zookeeper.connect", "52.24.35.74:2181");
 		properties.setProperty("group.id", "flink_SmartHome_Consumer");
-		
-		DataStream<String> stream = env.addSource(new FlinkKafkaConsumer09<>( "SmartHomeMeterTopic", new SimpleStringSchema(), properties) );
 
-		DataStream<Tuple6<Integer,Date,String,String,Float,Float>> cep_map = stream.flatMap(new FlatMapFunction<String, Tuple6<Integer,Date,String,String,Float,Float>>() {
-            		@Override
-            		public void flatMap(String s, Collector<Tuple6<Integer,Date,String,String,Float,Float>> collector) throws Exception {
-            		    Gson gson = new Gson();
-            		    Map<String, String> map = new HashMap<String, String>();
-            		    Map<String, String> myMap = gson.fromJson(s, map.getClass());
 
-			    String[] event_class = {"Medium", "Low", "High", "Non-event"};
-			    Random random = new Random();
-			    int index = random.nextInt(event_class.length);
+		DataStream<String> stream = env.
+			addSource(new FlinkKafkaConsumer09<>( 
+				"SmartHomeMeterTopic", 
+				new SimpleStringSchema(), 
+				properties));
 
-            		    DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-            		    Date date = format.parse(myMap.get("time"));
+//		DataStream<Tuple6<Integer,Date,String,String,Float,Float>> cepMap = stream.flatMap(new FlatMapFunction<String, Tuple6<Integer,Date,String,String,Float,Float>>() {
+//			@Override
+//			public void flatMap(String s, Collector<Tuple6<Integer,Date,String,String,Float,Float>> collector) throws Exception {
+//				Gson gson = new Gson();
+//				Map<String, String> map = new HashMap<String, String>();
+//				Map<String, String> myMap = gson.fromJson(s, map.getClass());
+//
+//			    	String[] event_class = {"Medium", "Low", "High", "Non-event"};
+//			    	Random random = new Random();
+//			    	int index = random.nextInt(event_class.length);
+//
+//				DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+//				Date date = format.parse(myMap.get("time"));
+//
+//				Integer home_id = Integer.parseInt(myMap.get("home_id"));
+//				Float latitude = Float.parseFloat(myMap.get("LAT"));
+//				Float longitude = Float.parseFloat(myMap.get("LONG"));
+//				//Float usage = Float.parseFloat(myMap.get("usage"));
+//				//Float generation = Float.parseFloat(myMap.get("generation"));
+//			    	String event_descr = "complex event";
+//			   	String event_severity = event_class[index];
+//            		    
+//			    	Tuple6<Integer,Date,String,String,Float,Float> t_u_map = new Tuple6<>();
+//				t_u_map.setFields(home_id, date, event_descr, event_severity, latitude, longitude);
+//				collector.collect(t_u_map);
+//			}
+//		});
 
-            		    Integer home_id = Integer.parseInt(myMap.get("home_id"));
-            		    Float latitude = Float.parseFloat(myMap.get("LAT"));
-            		    Float longitude = Float.parseFloat(myMap.get("LONG"));
-			    String event_descr = "complex event";
-			    String event_severity = event_class[index];
+
+		DataStream<Tuple8<Integer,Date,String,String,Float,Float,Float,Float>> cepMap = stream
+			.flatMap(new FlatMapFunction<String, Tuple8<Integer,Date,String,String,Float,Float,Float,Float>>() {
+			
+			@Override
+			public void flatMap(String s, Collector<Tuple8<Integer,Date,String,String,Float,Float,Float,Float>> collector) throws Exception {
+				Gson gson = new Gson();
+				Map<String, String> map = new HashMap<String, String>();
+				Map<String, String> myMap = gson.fromJson(s, map.getClass());
+
+			    	String[] event_class = {"Medium", "Low", "High", "Non-event"};
+			    	Random random = new Random();
+			    	int index = random.nextInt(event_class.length);
+
+				DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+				Date date = format.parse(myMap.get("time"));
+
+				Integer home_id = Integer.parseInt(myMap.get("home_id"));
+				Float latitude = Float.parseFloat(myMap.get("LAT"));
+				Float longitude = Float.parseFloat(myMap.get("LONG"));
+				Float usage = Float.parseFloat(myMap.get("usage"));
+				Float generation = Float.parseFloat(myMap.get("generation"));
+			    	String event_descr = "complex event";
+			   	String event_severity = event_class[index];
             		    
-			    Tuple6<Integer,Date,String,String,Float,Float> t_u_map = new Tuple6<>();
-            		    t_u_map.setFields(home_id, date, event_descr, event_severity, latitude, longitude);
+			    	Tuple8<Integer,Date,String,String,Float,Float,Float,Float> t_u_map = new Tuple8<>();
+				t_u_map.setFields(home_id, date, event_descr, event_severity, latitude, longitude, usage, generation);
+				
 
-            		    collector.collect(t_u_map);
-            		}
-        	});
+				collector.collect(t_u_map);
+			}
+		});
 
-		CassandraSink.addSink(cep_map)
-        	        .setQuery(INSERT_CEP)
-        	        .setClusterBuilder(new ClusterBuilder() {
-        	            @Override
-        	            protected Cluster buildCluster(Builder builder) {
-        	                return builder.addContactPoint("10.0.0.10").build();
-        	            }
-        	        }).build();
+		DataStream<Tuple8<Integer,Date,String,String,Float,Float,Float,Float>> cepMapTimedValue = 
+			cepMap.assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple8<Integer,Date,String,String,Float,Float,Float,Float>>() {
+        		@Override
+        		public long extractAscendingTimestamp(Tuple8<Integer,Date,String,String,Float,Float,Float,Float> element) {
+				long timeMs = element.f1.getTime();
+        		    	return timeMs;
+        		}
+		});
+
+
+		DataStream<Tuple8<Integer,Date,String,String,Float,Float,Float, Float>> cepMapByHomeId = cepMapTimedValue.keyBy(0);
+
+		//cepMapByHomeId.print();
+
+
+		Pattern<Tuple8<Integer,Date,String,String,Float,Float,Float,Float>, ?> cep1 =
+				Pattern.<Tuple8<Integer,Date,String,String,Float,Float,Float,Float>>begin("start")
+				.where(new SimpleCondition<Tuple8<Integer, Date, String, String, Float, Float, Float, Float>>() {
+					@Override
+					public boolean filter(Tuple8<Integer, Date, String, String, Float, Float, Float, Float> event1) throws Exception {
+						return event1.f6 > 8.0f;
+						//Float overLowThreshold = 0.6f * event1.f7;
+						//if (event1.f6 >= overLowThreshold)
+						//	return true;
+						//else
+						//	return false;
+					}
+				});
+				//.followedBy("end")
+				//.where(new SimpleCondition<Tuple8<Integer, Date, String, String, Float, Float, Float, Float>>() {
+				//	@Override
+				//	public boolean filter(Tuple8<Integer, Date, String, String, Float, Float, Float, Float> event2) throws Exception {
+				//		Float overHighThreshold = 0.8f * event2.f7;
+				//		if (event2.f6 >= overHighThreshold)
+				//			return true;
+				//		else
+				//			return false;
+				//	}
+				//});
+
+		PatternStream<Tuple8<Integer, Date, String, String, Float, Float, Float, Float>> patternStream = CEP.pattern(cepMapByHomeId, cep1);
+
+
+		DataStream<Tuple6<Integer, Date, String, String, Float, Float>> alerts = patternStream
+			.select(new PatternSelectFunction<Tuple8<Integer,Date,String,String,Float,Float,Float,Float>, Tuple6<Integer, Date, String, String, Float, Float>>() {
+
+
+			@Override
+			public Tuple6<Integer, Date, String, String, Float, Float> select(Map<String, List<Tuple8<Integer, Date, String, String, Float, Float, Float, Float>>> pattern) throws Exception {
+
+				Tuple8<Integer, Date, String, String, Float, Float, Float, Float>	endEvent = pattern.get("start").get(0);
+				Tuple6<Integer, Date, String, String, Float, Float> outEvent = new Tuple6<>();
+				
+				outEvent.setField(endEvent.getField(0), 0);
+				outEvent.setField(endEvent.getField(1), 1);
+				outEvent.setField(endEvent.getField(2), 2);
+				outEvent.setField(endEvent.getField(3), 3);
+				outEvent.setField(endEvent.getField(4), 4);
+				outEvent.setField(endEvent.getField(5), 5);
+
+				return outEvent;
+			}
+
+
+		});
+
+		//alerts.print();
+
+		CassandraSink.addSink(alerts)
+				.setQuery(INSERT_CEP)
+				.setClusterBuilder(new ClusterBuilder() {
+					@Override
+					protected Cluster buildCluster(Builder builder) {
+						return builder.addContactPoint("10.0.0.10").build();
+					}
+				}).build();
 
 
 
@@ -146,58 +263,36 @@ public class StreamingJob {
         	//         }).build();
 
 
-//        SingleOutputStreamOperator<String> time_and_usage = stream.map(new MapFunction<String, String>() {
-//		  private static final long serialVersionUID = -6867736771747690202L;
-//
-//		  @Override
-//		  public String map(String value) throws Exception {
-//		      	Gson gson = new Gson();
-//              	Map<String, String> map = new HashMap<String, String>();
-//             	 	Map<String, String> myMap = gson.fromJson(value, map.getClass());
-//
-//		    return myMap.get("time")+","+myMap.get("usage");
-//		  }
-//		});
-//
-//		// Adding cassandra sink
-//		DataStreamSource<Tuple2<String, Integer>> source = env.fromCollection(collection);
-//
-//
-//
-//		CassandraSink.addSink(source)
-//			.setQuery(INSERT)
-//			.setClusterBuilder(new ClusterBuilder() {
-//				@Override
-//				protected Cluster buildCluster(Builder builder) {
-//					return builder.addContactPoint("10.0.0.10").build();
-//				}
-//			})
-//			.build();
-
-		env.execute("WriteTupleIntoCassandra");
+		//  SingleOutputStreamOperator<String> time_and_usage = stream.map(new MapFunction<String, String>() {
+		//	  private static final long serialVersionUID = -6867736771747690202L;
+		//
+		//	  @Override
+		//	  public String map(String value) throws Exception {
+		//	      	Gson gson = new Gson();
+		//        	Map<String, String> map = new HashMap<String, String>();
+		//       	 	Map<String, String> myMap = gson.fromJson(value, map.getClass());
+		//
+		//	    return myMap.get("time")+","+myMap.get("usage");
+		//	  }
+		//	});
+		//
+		//	// Adding cassandra sink
+		//	DataStreamSource<Tuple2<String, Integer>> source = env.fromCollection(collection);
+		//
+		//
+		//
+		//	CassandraSink.addSink(source)
+		//		.setQuery(INSERT)
+		//		.setClusterBuilder(new ClusterBuilder() {
+		//			@Override
+		//			protected Cluster buildCluster(Builder builder) {
+		//				return builder.addContactPoint("10.0.0.10").build();
+		//			}
+		//		})
+		//		.build();
+		
+		//		env.execute("WriteTupleIntoCassandra");
 		// Added cassandra sink
-
-
-
-		/**
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
 
 		// execute program
 		env.execute("Flink Streaming Java API Skeleton");
