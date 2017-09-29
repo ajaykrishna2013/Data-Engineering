2971708a304b99f82388ca7f2253e3b47984352a
Ajay <ajaykrishna@gmail.com>
2017-09-29 03:39:37 +0000

    update producer script

--- CEPAnalyticsPipeline/data/SmartHomeProducer.py
+++ CEPAnalyticsPipeline/data/SmartHomeProducer.py
@@ -5,37 +5,51 @@ import os
 from datetime import datetime, timedelta
 import json
 import time
+import sys
+import random
 
 class SmartHomeProducer():
-	def __init__(self):
+	def __init__(self, kafka_cfg):
         	self.step = timedelta(seconds=1)
         	self.duration = timedelta(days=1)
 		self.startTime = datetime.now()
 		self.endTime = self.startTime + self.duration
-		self.proc = Producer({'bootstrap.servers':'54.69.161.38:9092'})
-		
-	def transmit_home_data(self):
-		with open('HomeAMeter.csv', 'rb') as f:
-			reader = csv.DictReader(f)
-			count = 0
-			for row in reader:
-				#print self.startTime
-				eventTime = self.startTime + self.step
-				eventTimestamp = str(eventTime)
-				row['time'] = eventTimestamp
-				#print row['time'], row['usage']
-				self.proc.produce('SmartHomeMeterTopic', json.dumps(row))
-				self.startTime = eventTime
-				count += 1
-				if count == 50000:
-					break		
-		return count
+		self.proc = Producer(kafka_cfg)
 
+	def transmit_home_data(self, infile):
+                with open(infile, 'rb') as f:
+                        reader = csv.DictReader(f)
+                        count = 0
+			total_count = 0
+                        allMeter = []
+                        for row in reader:
+                                allMeter.append(row)
+                        for row in allMeter:
+                                count += 1
+                                eventTime = self.startTime + self.step
+                                eventTimestamp = str(eventTime)
+                                row['time'] = eventTimestamp
+                                self.proc.produce('SHMeterTopic2', json.dumps(row))
+                                self.startTime = eventTime
+                                if count == 1000:
+					total_count += count
+					if total_count == 40000: break
+                                        delay = random.uniform(0,2)
+                                        time.sleep(delay)
+					count = 0
+                return total_count    
 
 if __name__ == '__main__':
-	home1 = SmartHomeProducer()
+	infile = sys.argv[1]
+	config = sys.argv[2]
+	kafka_config = {}
+	with open(config, 'r') as cfg:
+		config = cfg.readlines()
+		server, address = config[0].rstrip('\n').split('=')
+	kafka_config[server] = address
+	home1 = SmartHomeProducer(kafka_config)
 	startTime = time.time()
-	count = home1.transmit_home_data()
+	count = home1.transmit_home_data(infile)
 	time_taken = time.time() - startTime
 	print 'sent %d events in %f' % (count, time_taken) 
 	
